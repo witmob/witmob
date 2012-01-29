935d4b833eeba7eda08c646cf61e1c69fc7199c6
wangjun <marcus.jun@qq.com>
2012-01-29 20:00:43 +0800

    添加Activity和Search框架代码

---
+++ witmob_office/LastViewedTableCell.m
@@ -0,0 +1,29 @@
+//
+//  LastViewedTableCell.m
+//  witmob_office
+//
+//  Created by marcus wang on 12-1-29.
+//  Copyright (c) 2012年 www.witmob.com. All rights reserved.
+//
+
+#import "LastViewedTableCell.h"
+
+@implementation LastViewedTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ witmob_office/WriteWorkContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  WriteWorkContentViewController.h
+//  witmob_office
+//
+//  Created by marcus wang on 12-1-29.
+//  Copyright (c) 2012年 www.witmob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WriteWorkContentViewController : UIViewController
+
+@end
