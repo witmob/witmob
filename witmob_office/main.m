bc1189c90418bca30ae656fd3213ae10dff4d8f0
marcus <marcus@wangjundeMacBook-Pro.local>
2012-01-29 11:07:59 +0800

    Initial Commit

---
+++ witmob_office/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  witmob_office
+//
+//  Created by marcus wang on 12-1-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OfficeAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OfficeAppDelegate class]));
+    }
+}
